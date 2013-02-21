344f242c809ff32aabe72d0b0210772c222cf36f
Masahiro Hayashi <mhayashi1120@gmail.com>
2013-02-22 01:18:30 +0900

    fix

--- piki-mode.el
+++ piki-mode.el
@@ -101,22 +101,49 @@
       (setq piki-font-lock-emphasis-regions nil)
       nil)))
 
+(defun piki-font-lock--pre ()
+  ;; TODO adhoc solution.
+  `((lambda (end-region)
+      (let ((inhibit-read-only t)
+            (flg (buffer-modified-p)))
+        (unwind-protect
+            (save-excursion
+              (let ((before (point-min)))
+                (goto-char (point-min))
+                (while (re-search-forward "^>|$" end-region t)
+                  (forward-line 1)
+                  (let ((start (point))
+                        (end (or (and (re-search-forward "^|<$" nil t)
+                                      (progn
+                                        (forward-line 0)
+                                        (point)))
+                                 end-region)))
+                    ;;TODO
+                    (put-text-property start end 'face 'piki-verbatim-face)
+                    (put-text-property start end 'font-lock-multiline t)
+                    (remove-text-properties before start '(fontified face))
+                    (setq before end)))
+                (remove-text-properties before end-region '(fontified face))))
+          (set-buffer-modified-p flg)))
+      nil)))
+
 (defun piki-fontify-later (face)
   `(ignore nil (setq piki-font-lock-emphasis-regions
-		     (cons (list (match-data) ,face)
-			   piki-font-lock-emphasis-regions))))
+                     (cons (list (match-data) ,face)
+                           piki-font-lock-emphasis-regions))))
 
 (defun piki-font-lock-fontify-delayed-regions ()
   '((lambda (end)
       (if piki-font-lock-emphasis-regions
-	  (let ((match (nth 0 (car piki-font-lock-emphasis-regions))))
-	    (set-match-data match)
-	    (goto-char (match-end 0))
-	    (match-beginning 0))))
+          (let* ((item (car piki-font-lock-emphasis-regions))
+                 (match (nth 0 item)))
+            (set-match-data match)
+            (goto-char (match-end 0))
+            (match-beginning 0))))
     (2 (prog1
-	   (nth 1 (car piki-font-lock-emphasis-regions))
-	 (setq piki-font-lock-emphasis-regions
-	       (cdr piki-font-lock-emphasis-regions)))
+           (nth 1 (car piki-font-lock-emphasis-regions))
+         (setq piki-font-lock-emphasis-regions
+               (cdr piki-font-lock-emphasis-regions)))
        append)))
 
 (defun piki-font-lock-matcher (regexp)
@@ -125,6 +152,12 @@
 (put 'piki-font-lock-matcher 'lisp-indent-function 1)
 
 (defvar piki-mode-map nil)
+
+(defun piki--re-title ()
+  )
+
+(defconst piki--re-title
+  "\\(\\(?:\"\\(?:\\\\\"\\|[^\"]\\)+\"\\)\\|[^ \t\n]+\\)")
 
 ;; TODO insert html tag region? ex: \<strong\><\/strong\>
 
@@ -136,96 +169,113 @@
 (defvar piki-font-lock-keywords nil
   "Default expressions to highlight in Piki modes.")
 
-;;TODO
+;;TODO move to defvar form
 (setq piki-font-lock-keywords
       `((eval . (piki-font-lock-init))
 
-	("^#.*" . font-lock-comment-face)
+        ;; first of all, fontify `pre' tag
+        (eval . (piki-font-lock--pre))
 
-	;; <pre>
-	("^\\(>|\\)\n\\(\\(?:.*\n\\)*?\\)\\(|<\\)$"
-	 (1 font-lock-keyword-face)
-	 (2 piki-verbatim-face t)	; override faces
-	 (3 font-lock-keyword-face))
+        ("^#.*" . font-lock-comment-face)
 
-	;; <hr>
-	("^=.*$" . piki-bold-face)
+        ;; <hr>
+        ("^=.*$" . piki-bold-face)
 
-	;; <ul> and <ol>
-	("^\\([-+]+\\)" (1 font-lock-function-name-face))
+        ;; <ul> and <ol>
+        ("^\\([-+]+\\)" (1 font-lock-function-name-face))
 
-	;; <table>
-	("^\\(|\\)\\(.*?\\)\\(|\\)$"
-	 (1 piki-table-border-face)
-	 (3 piki-table-border-face)
-	 ("|"
-	  (progn
-	    (goto-char (match-beginning 2))
-	    (match-end 2))
-	  (goto-char (match-end 0))
-	  (0 piki-table-rule-face)))
+        ;; <table>
+        ("^\\(|\\)\\(.*?\\)\\(|\\)$"
+         (1 piki-table-border-face)
+         (3 piki-table-border-face)
+         ("|"
+          (progn
+            (goto-char (match-beginning 2))
+            (match-end 2))
+          (goto-char (match-end 0))
+          (0 piki-table-rule-face)))
 
-	;; <dt>
-	("^\\(\\?.*\\)"
-	 (1 font-lock-keyword-face))
+        ;; <dt>
+        ("^\\(\\?.*\\)"
+         (1 font-lock-keyword-face))
 
-	;; <dd>
-	("^\\(\\!.*\\)"
-	 (1 font-lock-keyword-face))
+        ;; <dd>
+        ("^\\(!.*\\)"
+         (1 font-lock-keyword-face))
 
-	;; (,(piki-font-lock-matcher	; <strong>
-	;;       (concat "\\('''\\)\\([^'\n]?"
-	;; 	      piki-any-char-regexp "*?\\)\\('''\\)"))
-	;;  (1 font-lock-keyword-face)
-	;;  ,(piki-fontify-later piki-bold-face)
-	;;  (3 font-lock-keyword-face))
-	;; (,(piki-font-lock-matcher	; <em>
-	;;       (concat "\\(''\\)\\([^'\n]?" piki-any-char-regexp "*?\\)\\(''\\)"))
-	;;  (1 font-lock-keyword-face)
-	;;  ,(piki-fontify-later piki-italic-face)
-	;;  (3 font-lock-keyword-face))
+        ;; (,(piki-font-lock-matcher    ; <strong>
+        ;;       (concat "\\('''\\)\\([^'\n]?"
+        ;;            piki-any-char-regexp "*?\\)\\('''\\)"))
+        ;;  (1 font-lock-keyword-face)
+        ;;  ,(piki-fontify-later piki-bold-face)
+        ;;  (3 font-lock-keyword-face))
+        ;; (,(piki-font-lock-matcher    ; <em>
+        ;;       (concat "\\(''\\)\\([^'\n]?" piki-any-char-regexp "*?\\)\\(''\\)"))
+        ;;  (1 font-lock-keyword-face)
+        ;;  ,(piki-fontify-later piki-italic-face)
+        ;;  (3 font-lock-keyword-face))
 
-	;; [url]
-	(,(piki-font-lock-matcher
-	      "\\(?:^\\|[^\\\\]\\)\\(\\[\\)\\(\\(?:\"[^\"]+\"\\)+\\|[^ \t\n]+\\)[ \t]\\(\\(?:\\\\]\\|[^]]\\)+\\)\\(\\]\\)")
-	 (1 font-lock-keyword-face)
-	 (2 font-lock-reference-face)
-	 (3 piki-link-face)
-	 (4 font-lock-keyword-face))
+        ;; [url]
+        (,(concat
+           ;; line begining or not escaped "["
+           "\\(?:^\\|[^\\\\]\\)"
+           ;; beginning of bracket
+           "\\(\\[\\)"
+           ;; title string or double quoted string
+           piki--re-title
+           ;; separator of title and link
+           "[ \t]+"
+           ;; link
+           "\\(\\(?:\\\\\]\\|[^\]]\\)+\\)"
+           ;; end of bracket
+           "\\(\\\]\\)"
+           )
+         (1 font-lock-keyword-face)
+         (2 font-lock-reference-face)
+         (3 piki-link-face)
+         (4 font-lock-keyword-face))
 
-	;; <img>
-	(,(piki-font-lock-matcher
-	      "^@ ?\\(\\(?:\"[^\"]+\"\\)+\\|[^ \t\n]+\\)[ \t]\\(.*\\)")
-	 (1 font-lock-reference-face)
-	 (2 piki-link-face))
+        ;; <img>
+        (,(concat
+           "^@"
+           "[ \t]*"
+           piki--re-title
+           "[ \t]+"
+           "\\(.*\\)"
+           )
+         (1 font-lock-reference-face)
+         (2 piki-link-face))
 
-	;; <h2>, <h3>, <h4>, ...
-	("^\\(\\*+\\).*"
-	 (0 (let* ((len (- (match-end 1) (match-beginning 1)))
-		   (face-name (format "piki-header-%d-face" (min len 5))))
-	      (intern face-name))
-	    keep))
+        ;; <h2>, <h3>, <h4>, ...
+        ("^\\(\\*+\\).*"
+         (0 (let* ((len (- (match-end 1) (match-beginning 1)))
+                   (face-name (format "piki-header-%d-face" (min len 5))))
+              (intern face-name))
+            keep))
 
-	;; fontify body portion of '''...''', ''...'', and :...:
-	;; These fontifications are delayed until here.
-	,(piki-font-lock-fontify-delayed-regions)
+        ;; <div>
+        ("^\\({\\)\\(.*\\)"
+         (1 font-lock-keyword-face)
+         (2 font-lock-function-name-face))
+        ("^\\(}\\)"
+         (1 font-lock-keyword-face))
 
-	;; ("^\\({\\)\\(?:\\([A-Z].*\\)\\|\\(.*\\)\\)"
-	("^\\({\\)\\(.*\\)"
-	 (1 font-lock-keyword-face)
-	 (2 font-lock-function-name-face))
-	("^\\(}\\)"
-	 (1 font-lock-keyword-face))
+        ;; <pre>
+        ("^\\(>|\\||<\\)$"
+         (1 font-lock-keyword-face))
+
+        ,(piki-font-lock-fontify-delayed-regions)
 
         ))
 
+;;;###autoload
 (define-derived-mode piki-mode text-mode "Piki"
   "Major mode to edit and commit Piki page.
 
 \\{piki-mode-map}"
   (interactive)
   (set (make-local-variable 'font-lock-defaults)
-       '(piki-font-lock-keywords t t nil backward-paragraph))
+       '(piki-font-lock-keywords t t))
   (set (make-local-variable 'font-lock-multiline) t)
   (setq major-mode 'piki-mode)
   (setq mode-name "Piki")
@@ -237,25 +287,25 @@
 (defun piki-font-lock-search-no-face (regexp &optional bound noerror face)
   (or bound (setq bound (point-max)))
   (let ((saved (point))
-	(move-to-beg-func (if face 'text-property-not-all 'text-property-any))
-	(move-to-end-func (if face 'text-property-any 'text-property-not-all))
-	found)
+        (move-to-beg-func (if face 'text-property-not-all 'text-property-any))
+        (move-to-end-func (if face 'text-property-any 'text-property-not-all))
+        found)
     (while (and (not found)
-		(re-search-forward regexp bound t)) ; search roughly
+                (re-search-forward regexp bound t)) ; search roughly
       (let* ((beg (or (funcall move-to-beg-func (match-beginning 0) bound
-			       'face face)
-		      bound))
-	     (end (or (funcall move-to-end-func beg bound 'face face) bound)))
-	(if (and (< beg (match-beginning 0))
-		 (< (match-end 0) end))
-	    (setq found (point))
-	  (goto-char beg)
-	  (if (re-search-forward regexp end 'move)
-	      (setq found (point))))))
+                               'face face)
+                      bound))
+             (end (or (funcall move-to-end-func beg bound 'face face) bound)))
+        (if (and (< beg (match-beginning 0))
+                 (< (match-end 0) end))
+            (setq found (point))
+          (goto-char beg)
+          (if (re-search-forward regexp end 'move)
+              (setq found (point))))))
     (or found
-	(cond ((null noerror) (error "search failed"))
-	      ((eq noerror t) (goto-char saved) nil)
-	      (t (goto-char bound) nil)))))
+        (cond ((null noerror) (error "search failed"))
+              ((eq noerror t) (goto-char saved) nil)
+              (t (goto-char bound) nil)))))
 
 (defun piki-recenter-and-fontify (&optional arg)
   "Center point in window, redisplay frame, and fontify the current buffer
